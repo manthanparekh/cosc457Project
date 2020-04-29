2572cb9977e7c101194de832cd64404734bd1a23
Jdweagly <47993752+Jdweagly@users.noreply.github.com>
2020-04-29 18:58:47 -0400

    testing my commit

--- src/Login/GuiTestJustin.java
+++ src/Login/GuiTestJustin.java
@@ -1,8 +1,11 @@
+package Login;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import Login.User;
 import java.sql.Connection; 
 import java.sql.DriverManager;
 import java.sql.ResultSet; 
