ddf47440e19d1dbb022b980b3553ac3740fb9c47
Haleluya Getachew <hollieg7@gmail.com>
2020-04-29 17:42:07 -0400

    Merge origin/master
    Conflicts:
    	src/ConnectDB.java

---
+++ src/Login/UserHome.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Login;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+class UserHome extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    UserHome frame = new UserHome();
+                    //frame.setSize(1650,1080);
+                    //frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+                    //frame.setUndecorated(true);
+                    frame.setVisible(true);
+                   
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public UserHome() {
+
+    }
+
+    /**
+     * Create the frame.
+     */
+    public UserHome(String userSes) {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(450, 190, 1014, 597);
+        setResizable(false);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        JButton btnNewButton = new JButton("Logout");
+        btnNewButton.setForeground(new Color(0, 0, 0));
+        btnNewButton.setBackground(UIManager.getColor("Button.disabledForeground"));
+        btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 39));
+        btnNewButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int a = JOptionPane.showConfirmDialog(btnNewButton, "Are you sure?");
+                // JOptionPane.setRootFrame(null);
+                if (a == JOptionPane.YES_OPTION) {
+                    dispose();
+                    UserLogin obj = new UserLogin();
+                    obj.setTitle("Login");
+                    obj.setVisible(true);
+                }
+                dispose();
+                UserLogin obj = new UserLogin();
+
+                obj.setTitle("Login");
+                obj.setVisible(true);
+
+            }
+        });
+        btnNewButton.setBounds(500, 118, 491, 114);   //bounds of the logout button
+        contentPane.add(btnNewButton);
+        JButton button = new JButton("Change-password\r\n");
+        button.setBackground(UIManager.getColor("Button.disabledForeground"));
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ChangePassword bo = new ChangePassword(userSes);
+                bo.setTitle("Change Password");
+                bo.setVisible(true);
+
+            }
+        });
+        button.setFont(new Font("Tahoma", Font.PLAIN, 35));
+        button.setBounds(500, 320, 491, 114);   //bounds of the change password button
+        contentPane.add(button);
+    }
+}
+    
+
